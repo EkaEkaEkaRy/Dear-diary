1c907494cd38283e1b44c631f70a3e8bfaafaf1e
Ryabova2020 <76618274+EkaEkaEkaRy@users.noreply.github.com>
2024-03-02 18:11:30 +0300

    Create encoder.js

---
+++ backend db/public/cipher/encoder.js
@@ -0,0 +1,37 @@
+var BigInt = require("big-integer");
+
+exports.encoded = (password) => {
+    let num_text = "";
+    for (let i = 0; i < password.length; i++) {
+        //for (let j = 0; j < 8 - password.charCodeAt(i).toString().length; j++) num_text += "0";
+        num_text += password.charCodeAt(i);
+    }
+    let x = num_text
+    let N = "98718284527"
+    let e = "3533"
+
+    return PowMod(x, e, N).toString().slice(0, 20);
+}
+
+function PowMod(x, e, N) {
+    var r = "";
+    var m = "";
+    for (let i = 0; i < x.length/8; i++)
+    {
+        m = "";
+        for (let j = 0; j < 8; j++) m += x[i*8 + j];
+        r += (F(Number(m), Number(e), Number(N)) % Number(N)).toString(16);
+    }
+    return r;
+}
+
+function F(m, e, N) {
+    if (e == 0) return 1;
+    if (e == 1) return m;
+    if (m > N) return F(m % N, e, N);
+    if (e % 2 == 0) return (F(m, e/ 2, N) % N) * (F(m, e/ 2, N) % N);
+    else { 
+        let n = (F(m, e - 1, N) % N) * (F(m, 1, N) % N)
+        return n;
+    }
+}
