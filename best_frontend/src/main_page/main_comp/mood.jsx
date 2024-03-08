8fb6268e96a78b7a59a64cfa59681c9f6c213ec1
Ryabova2020 <76618274+EkaEkaEkaRy@users.noreply.github.com>
2024-03-08 13:28:57 +0300

    Update mood.jsx

--- best_frontend/src/main_page/main_comp/mood.jsx
+++ best_frontend/src/main_page/main_comp/mood.jsx
@@ -8,6 +8,7 @@ import up_down from './emotions/up-down.png'
 import sad from './emotions/sadness.png'
 import anxious from './emotions/anxious.png'
 import no_energy from './emotions/low_battery.png'
+import {useState} from "react";
 
 const moods = [
     {name: 'Радость'},
@@ -21,11 +22,79 @@ const moods = [
     {name: 'Нет энергии'},
 ];
 const Names = (props) => {
+    let func = props.onClick
+    let name = props.name
     return (
-        <label className={m.checkbox}><input className={m.box} type="checkbox"/>{props.name}</label>
+        <label className={m.checkbox}><input className={m.box} onClick={func} name={name} type="checkbox"/>{props.name}</label>
     )
 }
+
+function contains(arr, elem) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === elem) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function delete_element(arr, elem) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === elem) {
+            return arr.splice(i, 1);
+        }
+    }
+    console.log("error")
+}
+
+const mood_isTrue = []
 const Mood = () => {
+
+    let [user, setuser] = useState({
+        id_user: localStorage.getItem('userMailId'),
+        task: "Ваши эмоции",
+        message: "",
+    })
+
+    let name, value;
+
+    const handlerChange = (event) =>
+    {
+        name = event.target.name;
+        if (contains(mood_isTrue, name))
+        {
+            value = false;
+            delete_element(mood_isTrue, name);
+        }
+        else {
+            value = true;
+            mood_isTrue.push(name);
+        }
+        setuser({...user, ["message"]: mood_isTrue.join(', ')})
+    }
+
+    const handlerSubmit = async (event) => {
+        event.preventDefault();
+        const {id_user, task, message} = user;
+            if (id_user !== "unknown_user") {
+            const res = await fetch('http://localhost:1337/api/daily_tasks', {
+                method: "POST",
+                headers: { "Accept": "application/json", "Content-Type":
+                "application/json" },
+                body: JSON.stringify({
+                id_user,
+                task,
+                message,
+                })
+            });
+            mood_isTrue.splice(0, -1);
+            const data = res.json();
+            if (res.status === 400 || !data) console.log("пользователь уже существует")
+            const form = document.forms["userTask"];
+            form.reset();
+        }
+    }
+
     return (
         <div className={m.mood_wrapper}>
             <header className={m.header}>
@@ -36,20 +105,21 @@ const Mood = () => {
                     Можешь отметить сколько угодно вариантов
                 </div>
                 <div className={m.moods}>
-                    <form>
+                    <form method="POST" name="userTask" onSubmit={handlerSubmit}>
                     <div className={m.list}>
-                        <div className={m.list_item}><img className={m.img} src={happy} alt="Радость" /><Names name="Радость"/></div>
-                        <div className={m.list_item}><img className={m.img} src={energy} alt="Много энергии" /><Names name="Много энергии"/></div>
-                        <div className={m.list_item}><img className={m.img} src={calm} alt="Спокойствие" /><Names name="Спокойствие"/></div>
-                        <div className={m.list_item}><img className={m.img} src={irrit} alt="Злость" /><Names name="Злость"/></div>
-                        <div className={m.list_item}><img className={m.img} src={apathy} alt="Апатия" /><Names name="Апатия"/></div>
-                        <div className={m.list_item}><img className={m.img} src={up_down} alt="Перепады настроения" /><Names name="Перепады настроения"/></div>
-                        <div className={m.list_item}><img className={m.img} src={sad} alt="Грусть" /><Names name="Грусть" /></div>
-                        <div className={m.list_item}><img className={m.img} src={anxious} alt="Тревожность" /><Names name="Тревожность"/></div>
-                        <div className={m.list_item}><img className={m.img} src={no_energy} alt="Нет энергии" /><Names name="Нет энергии"/></div>
+                        <div className={m.list_item}><img className={m.img} src={happy} alt="Радость" /><Names name="Радость" onClick={handlerChange}/></div>
+                        <div className={m.list_item}><img className={m.img} src={energy} alt="Много энергии" /><Names name="Много энергии" onClick={handlerChange}/></div>
+                        <div className={m.list_item}><img className={m.img} src={calm} alt="Спокойствие" /><Names name="Спокойствие" onClick={handlerChange}/></div>
+                        <div className={m.list_item}><img className={m.img} src={irrit} alt="Злость" /><Names name="Злость" onClick={handlerChange}/></div>
+                        <div className={m.list_item}><img className={m.img} src={apathy} alt="Апатия" /><Names name="Апатия" onClick={handlerChange}/></div>
+                        <div className={m.list_item}><img className={m.img} src={up_down} alt="Перепады настроения" /><Names name="Перепады настроения" onClick={handlerChange}/></div>
+                        <div className={m.list_item}><img className={m.img} src={sad} alt="Грусть" /><Names name="Грусть" onClick={handlerChange}/></div>
+                        <div className={m.list_item}><img className={m.img} src={anxious} alt="Тревожность" /><Names name="Тревожность" onClick={handlerChange}/></div>
+                        <div className={m.list_item}><img className={m.img} src={no_energy} alt="Нет энергии" /><Names name="Нет энергии" onClick={handlerChange}/></div>
                     </div>
-                    </form>
                     <input className={m.button} type="submit" value={"Отправить"} />
+                    </form>
+                    
                 </div>
             </main>
         </div>
