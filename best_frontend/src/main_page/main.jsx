0af498f3b0e414a841ddf50d26318ee35f6e2550
Ryabova2020 <76618274+EkaEkaEkaRy@users.noreply.github.com>
2024-03-08 09:33:23 +0300

    Update main.jsx

--- best_frontend/src/main_page/main.jsx
+++ best_frontend/src/main_page/main.jsx
@@ -1,19 +1,25 @@
 import Header from "./main_comp/header_m"
-import Nav from "./main_comp/nav"
+//import Nav from "./main_comp/nav"
 import Left from './main_comp/left'
 import s from "./main.module.css"
-import { useEffect, useState } from "react"
-import { Navigate } from "react-router-dom"
+//import { useEffect, useState } from "react"
+//import { Navigate } from "react-router-dom"
 
 const Main = () => {
+  /*
   const [authenticated, setauthenticated] = useState(null);
   useEffect(() => {
-    const loggedInUser = localStorage.getItem("authenticated");
+    const loggedInUser = localStorage.getItem(authenticated);
+    console.log(loggedInUser)
     if (loggedInUser) {
       setauthenticated(loggedInUser);
     }
-  }, []);
-
+    }, []);
+    
+  if (!authenticated) {
+    return <Navigate replace to="/srart" />;
+  } else {
+    */
     return (
       // здесь я передаю имя пользователя в name в компонент header в качестве props
       <div className={s.app_wrapper}> 
@@ -24,5 +30,4 @@ const Main = () => {
       </div>
     )
   }
-
 export default Main
