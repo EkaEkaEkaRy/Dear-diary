02b4287deffa592981a5d6683faa82900d384430
Ryabova2020 <76618274+EkaEkaEkaRy@users.noreply.github.com>
2024-03-08 11:55:35 +0300

    Update header_m.jsx

--- best_frontend/src/main_page/main_comp/header_m.jsx
+++ best_frontend/src/main_page/main_comp/header_m.jsx
@@ -4,6 +4,18 @@ import { Link, NavLink } from "react-router-dom"
 
 const Header = (props) => {
     var userName = props.name; // здесь я присваиваю переменной значение из name из props (можно и без этого, но так удобнее)
+    if (localStorage.getItem('userMailId') === "unknown_user")
+    return (
+        // здесь я передаю имя пользователя в state на страницу profile
+        <header className={s.header}>
+            <NavLink to="/Start" state={{name: userName}}> 
+                <div className={s.profile}>
+                    <img className={s.ava} src={profile} alt="АВА" />
+                </div>
+            </NavLink>
+        </header>
+    )
+    else
     return (
         // здесь я передаю имя пользователя в state на страницу profile
         <header className={s.header}>
