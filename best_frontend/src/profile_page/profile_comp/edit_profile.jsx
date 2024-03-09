aca2dfad85af486a874c77c9658c6a123a767194
Ryabova2020 <76618274+EkaEkaEkaRy@users.noreply.github.com>
2024-03-09 13:19:31 +0300

    edit profile

--- best_frontend/src/profile_page/profile_comp/edit_profile.jsx
+++ best_frontend/src/profile_page/profile_comp/edit_profile.jsx
@@ -8,8 +8,7 @@ const Edit_form = () => {
     //const [authenticated, setauthenticated] = useState(false);
 
     let [user, setuser] = useState({
-        name: "",
-        mail: "",
+        name: JSON.parse(localStorage.getItem('userFormInfo')).name,
         password: "",
         password2: "",
     })
@@ -30,18 +29,24 @@ const Edit_form = () => {
         setuser({ ...user, [name]: value})
     }
 
+    const delete_accaunt = async (event) => {
+        event.preventDefault();
+    }
+
     const handlerSubmit = async (event) => {
         event.preventDefault();
-        const {name, mail, password, password2} = user;
+        const {name, password, password2} = user;
         if (password !== password2) document.getElementById("answer_for_user").innerHTML = "Пароли не совпадают"
         else {
+            let mailuserID = String(localStorage.getItem('userMailId'))
+            console.log(mailuserID)
             const res = await fetch('http://localhost:1337/api/users', {
                 method: "PUT",
                 headers: { "Accept": "application/json", "Content-Type":
                 "application/json" },
                 body: JSON.stringify({
+                mailuserID,
                 name,
-                mail,
                 password,
                 })
             });
@@ -50,31 +55,29 @@ const Edit_form = () => {
             if (res.status === 404 || !data) document.getElementById("answer_for_user").innerHTML = "Пользователь уже существует"
             else if (res.status === 500) document.getElementById("answer_for_user").innerHTML = "Попробуйте позже"
             //setauthenticated(true)
-            else {
-                localStorage.setItem('userMailId', mail);
-                navigate("/Main");
+            else if (res.status === 200){
+                localStorage.setItem('userNameId', name);
+                navigate('/Profile')
             }
 
             }
             
     };
     return (
-        <div className={l.signup}>
+        <div>
             <form class="form_auth" action="#" method="POST" name="userSignup" onSubmit={handlerSubmit}>
-                <div className={l.title}>Регистрация</div>
                 <div className={l.login_wrapper}>
                     <div><input className={l.input} name="name" type="text" placeholder='Имя' value = {user.name} onChange={handlerChange} required /></div>
-                    <div><input className={l.input} name="mail" type="email" placeholder='Почта' value = {user.mail} onChange={handlerChange} required /></div>
                     <div><input className={l.input} name="password" type="password" placeholder='Пароль' value = {user.password} onChange={handlerChange} required /></div>
                     <div><input className={l.input} name="password2" type="password" placeholder='Повторный пароль' value = {user.password2} onChange={handlerChange} required /></div>
                     <div id="answer_for_user"></div>
                     <div>
-                            <input className={l.button} type="submit" value={"Создать аккаунт"} />
-
+                            <input className={l.button} type="submit" value={"Сохранить изменения"} />
                     </div>
                 </div>
             </form>
-        </div>
+            <input className={l.button} onClick={delete_accaunt} value={"Удалить аккаунт"} />
+        </div>    
     )
 }
 
--- best_frontend/src/profile_page/profile_comp/settings.jsx
+++ best_frontend/src/profile_page/profile_comp/settings.jsx
@@ -1,5 +1,7 @@
 import s from './settings.module.css'
 import { useNavigate } from 'react-router-dom'
+import Edit_form from "./edit_profile"
+import { useState } from 'react'
 
 const SButton = (props) => {
     return (
@@ -8,18 +10,45 @@ const SButton = (props) => {
 }
 const Settings = () => {
     const navigate = useNavigate();
+    const [user_form, setUser_form] = useState(false)
     const handlerSubmit = async (event) => {
         event.preventDefault();
         localStorage.setItem('userMailId', null)
         navigate("/Start");
     }
+
+    const edit_form = async (event) => {
+        event.preventDefault();
+        const id_user = localStorage.getItem('userMailId');
+        const res = await fetch('http://localhost:1337/api/edit', {
+            method: "POST",
+            headers: { "Accept": "application/json", "Content-Type":
+            "application/json" },
+            body: JSON.stringify({
+            id_user,
+            })
+        });
+        const data = await res.json();
+        console.log(res.ok)
+        if (res.status === 400 || !data) console.log("пользователя с таким именем не существует")
+        else if (res.ok){
+            localStorage.setItem('userFormInfo', JSON.stringify(data))
+        }
+        setUser_form(!user_form)
+    }
+    if (user_form) {
+        return (<div className={s.settings_wrapper} id="settings">
+            <Edit_form />
+        </div>)
+    }
+    else
     return (
-        <div className={s.settings_wrapper}>
+        <div className={s.settings_wrapper} id="settings">
             <header className={s.header}>
                 Настройки
             </header>
             <ul className={s.list}>
-                <li className={s.list_item + ' ' + s.acc}><SButton val={"Настройки аккаунта"} /></li>
+                <li className={s.list_item + ' ' + s.acc}><SButton val={"Настройки аккаунта"} onClick={edit_form}/></li>
                 <li className={s.list_item + ' ' + s.theme}><SButton val={"Настройки темы"} /></li>
                 <li className={s.list_item + ' ' + s.leave}><SButton val={"Выход"} onClick={handlerSubmit}/></li>
             </ul>
